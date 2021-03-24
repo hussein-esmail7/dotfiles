360186f21d09532b95bb69cca8cf7af4bf63e22e
Hussein Esmail <hussein.esmail7@gmail.com>
2021-03-24 12:28:14 -0400

    Added script

---
+++ autostart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# autostart.sh
+# hussein
+# Created: 2021 03 24
+# Updated: 2021 03 24
+# Description: This file is used to autostart programs when booting into my Linux computer.
+
+
+sxhkd&
+transmission-daemon&
--- linux.sh
+++ linux.sh
@@ -9,20 +9,30 @@ sudo -v
 
 # Cool site where you can search available packages:
 # https://archlinux.org/packages/
-pacman -S texlive-most textlive-lang    # LaTeX Tools
+pacman -S texlive-most                  # LaTeX Tools
 pacman -S cmatrix                       # Cuz it looks cool
 pacman -S newsboat                      # My default RSS reader
 pacman -S steam
+pacman -S davinci-resolve
+pacman -S discord
+pacman -S minecraft-launcher
+pacman -S ttf-ms-fonts
+pacman -S zoom
 pacman -S vlc
 pacman -S gimp
 pacman -S code                          # VS Code
 pacman -S calibre
 pacman -S discord
-pacman -S ffmpeg
-pacman -S vivaldi                       # My default browser
-pacman -S python
 pacman -S youtube-dl
 pacman -S snapd                         # Install snapd store
+pacman -S brave                         # Default browser
+pacman -S calibre                       # ebook manager
+pacman -S chromium                      # For when Brave doesn't work
+pacman -S lollypop                      # Music player
+pacman -S neofetch
+pacman -S obs-studio                    # Screen record
+pacman -S thunderbird                   # Email client
+pacman -S yay-git                       # For AUR
 
 # Setup snapd
 systemctl enable --now snapd.socket
@@ -31,12 +41,19 @@ ln -s /var/lib/snapd/snap /snap
 # https://snapcraft.io
 snap install teams-for-linux            # Install Microsoft Teams
 
+sudo git clone https://aur.archlinux.org/yay-git.git
+sudo chown -R tecmint:tecmint ./yay-git
+cd yay-git
+makepkg -si
+sudo yay -Syu # Upgrade all yay packages
+sudo pacman -Syu # Upgrade all pacman packages
+sudo yay -S gparted
+
+
 # Other apps I would still need to install (terminal installation commands not found)
 # megasync
-# Minecraft
 # Spotify
 # Whatsapp
-# Zoom.us
 
 
 
