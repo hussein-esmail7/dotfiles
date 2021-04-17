75170ac831557954ba037f3763cef9018025506f
Hussein Esmail <hussein.esmail7@gmail.com>
2021-04-17 00:42:25 -0400

    modified:   linux.sh

--- linux.sh
+++ linux.sh
@@ -4,67 +4,63 @@
 # This has not been tested before, so keep that in mind if you intend to use this
 # If you see anything that can be improved/fixed, please create a pull request :)
 
+echo -n "Email for git: "
+read GIT_EMAIL
+
+echo -n "Name for git: "
+read GIT_NAME
+
 # Ask for user password immediately so it doesn't keep asking multiple times
 sudo -v
 
 # Cool site where you can search available packages:
 # https://archlinux.org/packages/
-pacman -S texlive-most                  # LaTeX Tools
-pacman -S cmatrix                       # Cuz it looks cool
-pacman -S newsboat                      # My default RSS reader
-pacman -S steam
-pacman -S davinci-resolve
-pacman -S discord
-pacman -S minecraft-launcher
-pacman -S ttf-ms-fonts
-pacman -S zoom
-pacman -S vlc
-pacman -S gimp
-pacman -S code                          # VS Code
-pacman -S discord
-pacman -S youtube-dl
-pacman -S snapd                         # Install snapd store
-pacman -S brave                         # Default browser
-pacman -S calibre                       # ebook manager
-pacman -S chromium                      # For when Brave doesn't work
-pacman -S neofetch
-pacman -S obs-studio                    # Screen recording software
-pacman -S thunderbird                   # Email client
-pacman -S yay-git                       # For AUR
-pacman -S mlocate                       # For 'locate' command
-pacman -S unrar
-pacman -S foobar
-pacman -S sxhkd                         # Mapping hotkey software
-pacman -S kitty                         # Printing images in terminal
-pacman -S xorg-xev                      # 'xev' command for monitoring keypresses
-pacman -S rhythmbox                     # Music player
-pacman -S toilet                        # Cool terminal fonts
-
 pacman -Syu # Upgrade all pacman packages
-
+pacman -S cmatrix newsboat-git steam davinci-resolve discord ttf-ms-fonts zoom vlc gimp discord youtube-dl snapd brave calibre chromium neofetch obs-studio thunderbird yay-git mlocate unrar foobar xorg-xev rhythmbox toilet vim amfora ueberzug
+# amfora                # Gemini page viewer
+# cmatrix               # Cuz it looks cool
+# newsboat              # My default RSS reader
+# davinci-resolve       # Video editor
+# ttf-ms-fonts          # Common fonts
+# zoom                  # Because who doesn't need this?
+# vlc                   # For when mpv doesn't work
+# mpv                   # Video viewer
+# youtube-dl            # YouTube video downloader
+# snapd                 # Install snapd store
+# brave                 # Default browser
+# calibre               # ebook manager
+# chromium              # For when Brave doesn't work
+# obs-studio            # Screen recording software
+# texlive-most          # LaTeX Tools
+# thunderbird           # Email client
+# yay-git               # For AUR
+# mlocate               # For 'locate' command
+# xorg-xev              # 'xev' command for monitoring keypresses
+# rhythmbox             # Music player
+# toilet                # Cool terminal fonts
+# ueberzug              # Images in terminal
 
 # Setup snapd
 systemctl enable --now snapd.socket
 ln -s /var/lib/snapd/snap /snap
 
-# https://snapcraft.io
-snap install teams-for-linux            # Install Microsoft Teams
-
 git clone https://aur.archlinux.org/yay-git.git
 sudo chown -R tecmint:tecmint ./yay-git
 cd yay-git
 makepkg -si
+
 yay -Syu # Upgrade all yay packages
-yay -S gparted
-yay -S musikcube
+yay -S gparted musikcube ytfzf minecraft-launcher spotify
+# gparted       # Disk partition manager
+# musikcube     # Terminal music player
+# ytfzf         # Search YouTube videos through Terminal
+# teams         # Microsoft Teams
 
-git config --global user.email "hussein.esmail7@gmail.com"
-git config --global user.name "Hussein Esmail"
-
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_NAME"
 
 # Other apps I would still need to install (terminal installation commands not found)
 # megasync
-# Spotify
 # Whatsapp
 
 
